4f85a70e85a2867a2c0c1669db58c93cfe1dfd71
RidwanC134 <ridwanhh10@gmail.com>
2023-03-27 09:19:23 +0700

    menampilkan rata-rata nilai lulus atau gagal

--- exerciseProsedural_134/exerciseProsedural_134.cpp
+++ exerciseProsedural_134/exerciseProsedural_134.cpp
@@ -5,6 +5,12 @@ double rerata(double a, double b) {
     return (a + b) / 2;
 }
 
+string status(double c) {
+    if (c < 70)
+        return "Lulus";
+    else 
+        return "Gagal";
+}
 
 int main()
 {
